943ff6c9a130091ea4c464d82ed3282a2a0b5ad3
mattyopl <90574735+mattyopl@users.noreply.github.com>
2025-02-12 21:42:32 -0500

    add source files pt1
    title

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cs4789-pa1",
+    version="0.0.1",
+    packages=["cs4789-pa1"],
+    python_requires=">=3.10",
+    install_requires=[
+        "numpy>=1.26.1",
+        "matplotlib>=3.8.0",
+    ],
+)
